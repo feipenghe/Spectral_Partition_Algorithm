dabdeb9a514926d0ea8e4e16b382bcc3ec15d1ed
murphy <hepengfe@uw.edu>
2020-12-14 01:03:32 -0800

    first commit

---
+++ spectral_partition.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import networkx as nx
+import numpy as np
+import gc
+import scipy
+
+
+G = nx.read_edgelist("com-amazon.ungraph.txt", create_using=nx.Graph, nodetype = int)
+n = G.number_of_nodes()
+L = nx.normalized_laplacian_matrix(G)
+
+A = nx.adjacency_matrix(G)
+
+
+# new
+D_diags = np.sum(A, axis = 1)
+D_diags = D_diags.squeeze(1)
+D_diags = D_diags.tolist()[0]
+D = scipy.sparse.diags(D_diags)
+
+
+# save memory
+del G
+
+gc.collect()
+
+
+I = scipy.sparse.diags([1]*n)
+
+one_vector = scipy.sparse.bsr_matrix([1]*n).transpose()
+
+v_1 = np.sqrt(D).dot(one_vector).todense()
+
+
+
+from scipy import stats
+x = scipy.stats.multivariate_normal.rvs(mean=0, cov=1, size=(n,1))
+x = scipy.sparse.csr_matrix(x)
+
+
+v_1_norm = np.linalg.norm(v_1, 2)
+y = x.transpose() - (x.dot(v_1*(1/v_1_norm)))[0,0]*(v_1*(1/v_1_norm))
+# print(y.shape)
+# print(x.dot(v_1*(1/v_1_norm))[0,0])
+# print((v_1*(1/v_1_norm)).shape)
+
+def power_method(y, M, k = 1000):
+    count = 0
+    while count < k:
+        y = y*(1/np.linalg.norm(y,2))
+        prev_y = y
+        y = M*y
+
+
+        update =  np.linalg.norm(y-prev_y, 2)
+        if count % 10 == 0:
+            print("update: ", update)
+        count += 1
+    eigenval = (y.transpose()@M@y)/(y.transpose()@y)
+    return y, eigenval
+M = (2*I-L)
+v_2, eigenval = power_method(y, M)
+
+D_csc = scipy.sparse.csc_matrix(D) # it's more efficient for spsolve
+
+
+
+scipy.sparse.diags(1/np.sqrt(D_csc.diagonal()))
+
+
+# In[75]:
+
+
+from scipy.sparse.linalg import spsolve
+
+
+vertices = scipy.sparse.diags(1/np.sqrt(D_csc.diagonal())) * v_2
+
+
+vertices_l = vertices.flatten().tolist()[0]
+
+indexed_vertices_l = list(enumerate(vertices_l))
+
+sorted_indexed_vertices_l = sorted(indexed_vertices_l, key = lambda t:t[1])
+
+
+def degree(i):
+    return D.data[0][i]
+D.data[0][298945]
+
+vol_V = np.sum(D.data)
+
+all_nodes_S = set(list(range(n)))
+S = set()
+conductance = 0
+vol_S = 0
+min_conductance = 99
+cut = 0
+for indexed_vertex in sorted_indexed_vertices_l:
+    v_index, v = indexed_vertex
+    num_intra = 0
+    num_inter = 0
+    for neighbour in A[v_index].indices: # loop over the neighbours and check if it's in S
+        if neighbour in S: 
+            num_intra += 1
+        else:
+            num_inter += 1
+    cut = cut - num_intra + num_inter
+    vol_S = vol_S + degree(v_index)
+    conductance = cut / min(vol_S, vol_V-vol_S)
+    S.add(v_index)
+    if conductance < min_conductance:
+        min_conductance = conductance
+        min_set = S if vol_S <= (vol_V-vol_S) else all_nodes_S - S
+        print("min_conductance: ", min_conductance)
+
+print("final min_conductance: ", min_conductance)
+print("the node ids of min set is: ", min_set)
+
+
+# no, keep the vertices matrix
+
+# extract the diagonal and label them
+# sorted( , lambda )
+# sweep
+# |E| - number of S intra edge + number of (S,T) inter edges
+# |vol| + degree
+
