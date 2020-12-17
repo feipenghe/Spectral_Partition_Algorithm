86ff966ff7a8c36a91b2b35278dc964a3773ce47
murphy <hepengfe@uw.edu>
2020-12-16 23:21:31 -0800

    fied the node id

--- spectral_partition.py
+++ spectral_partition.py
@@ -8,10 +8,21 @@ import scipy
 
 
 G = nx.read_edgelist("com-amazon.ungraph.txt", create_using=nx.Graph, nodetype = int)
-n = G.number_of_nodes()
-L = nx.normalized_laplacian_matrix(G)
+n = G.number_of_nodes() # use this but in a ordered way
+print(list(G.nodes)[:10])
 
-A = nx.adjacency_matrix(G)
+nodes = sorted(list(G.nodes))
+# n = max(list(G.nodes))
+# print("n: ", n)
+# print("m: ", m)
+# exit()
+L = nx.normalized_laplacian_matrix(G,  nodelist=nodes)
+
+A = nx.adjacency_matrix(G, nodelist=nodes) # zero based
+# to do create a node id -> row id, dictionary
+row2id = dict( enumerate(nodes))  # zero-based
+id2row =  dict( (node_id, row) for (row, node_id) in row2id.items())
+
 
 
 # new
@@ -21,10 +32,10 @@ D_diags = D_diags.tolist()[0]
 D = scipy.sparse.diags(D_diags)
 
 
-# save memory
-del G
-
-gc.collect()
+# # save memory
+# del G
+#
+# gc.collect()
 
 
 I = scipy.sparse.diags([1]*n)
@@ -81,7 +92,7 @@ vertices = scipy.sparse.diags(1/np.sqrt(D_csc.diagonal())) * v_2
 
 vertices_l = vertices.flatten().tolist()[0]
 
-indexed_vertices_l = list(enumerate(vertices_l))
+indexed_vertices_l = list(enumerate(vertices_l))  # row index and value
 
 sorted_indexed_vertices_l = sorted(indexed_vertices_l, key = lambda t:t[1])
 
@@ -94,30 +105,33 @@ vol_V = np.sum(D.data)
 
 all_nodes_S = set(list(range(n)))
 S = set()
+# set(G.nodes)
 conductance = 0
 vol_S = 0
 min_conductance = 99
 cut = 0
 for indexed_vertex in sorted_indexed_vertices_l:
-    v_index, v = indexed_vertex
+    v_row_idx, v = indexed_vertex
+
     num_intra = 0
     num_inter = 0
-    for neighbour in A[v_index].indices: # loop over the neighbours and check if it's in S
+    for neighbour in A[v_row_idx].indices: # loop over the neighbours and check if it's in S
+        # neighbour is node id
         if neighbour in S: 
             num_intra += 1
         else:
             num_inter += 1
     cut = cut - num_intra + num_inter
-    vol_S = vol_S + degree(v_index)
+    vol_S = vol_S + degree(v_row_idx)
     conductance = cut / min(vol_S, vol_V-vol_S)
-    S.add(v_index)
+    S.add(v_row_idx)
     if conductance < min_conductance:
         min_conductance = conductance
         min_set = S if vol_S <= (vol_V-vol_S) else all_nodes_S - S
         print("min_conductance: ", min_conductance)
 
 print("final min_conductance: ", min_conductance)
-print("the node ids of min set is: ", min_set)
+print("the node ids of min set is: ", [row2id[row_idx] for row_idx in min_set])
 
 
 # no, keep the vertices matrix
